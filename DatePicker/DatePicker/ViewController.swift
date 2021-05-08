7afe155d2477a0e44fd909ec2ff6a6f37db85e5f
Kim <hyunjin1612@gmail.com>
2021-05-08 13:06:38 +0900

    알람기능 추가, 리팩토링

--- DatePicker/DatePicker/ViewController.swift
+++ DatePicker/DatePicker/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     let timeSelector: Selector = #selector(ViewController.updateTime)
     let interval = 1.0
+    var alarmTime: String?
     
     @IBOutlet var lblCurrentTime: UILabel!
     @IBOutlet var lblPickerTime: UILabel!
@@ -23,16 +24,43 @@ class ViewController: UIViewController {
 
     @IBAction func changeDatePicker(_ sender: UIDatePicker) {
         let datePickerView = sender
-        let dateFormatter: DateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
-        lblPickerTime.text = "선택시간: " + dateFormatter.string(from: datePickerView.date)
+        changePickerTimeLabel(date: datePickerView.date)
+        setAlarmTime(date: datePickerView.date)
     }
     
     // #selector()의 인자로 사용할 메서드는 Objective-C와의 호환성을 위하여 함수 앞에 @objc키워드를 붙여야한다.
     @objc func updateTime() {
+        let currentTime: Date = NSDate() as Date
+        changeCurrentTimeLabel(date: currentTime)
+        alarm(date: currentTime)
+    }
+    
+    func changePickerTimeLabel(date: Date) {
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        lblPickerTime.text = "선택시간: " + dateFormatter.string(from: date)
+    }
+    
+    func changeCurrentTimeLabel(date: Date) {
         let dateFormatter: DateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
-        lblCurrentTime.text = "현재시간: " + dateFormatter.string(from: NSDate() as Date)
+        lblCurrentTime.text = "현재시간: " + dateFormatter.string(from: date)
+    }
+    
+    func setAlarmTime(date: Date) {
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm aaa"
+        alarmTime = dateFormatter.string(from: date)
+    }
+    
+    func alarm(date: Date) {
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm aaa"
+        if dateFormatter.string(from: date) == alarmTime {
+            view.backgroundColor = UIColor.red
+        } else {
+            view.backgroundColor = UIColor.white
+        }
     }
 }
 
