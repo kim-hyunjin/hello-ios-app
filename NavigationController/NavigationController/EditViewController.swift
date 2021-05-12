b7cf2045fd7288af3b3e30d519e28796e3726a65
Kim <hyunjin1612@gmail.com>
2021-05-12 22:48:21 +0900

    네비게이션 컨트롤러, 화면간 데이터 주고받기, delegate 프로토콜 사용

--- NavigationController/NavigationController/EditViewController.swift
+++ NavigationController/NavigationController/EditViewController.swift
@@ -7,21 +7,72 @@
 
 import UIKit
 
+protocol EditDelegate {
+    func didMessageEditDone(_ controller: EditViewController, message: String)
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool)
+    func didImageZoomDone(_ controller: EditViewController, isZoom: Bool)
+}
+
 class EditViewController: UIViewController {
     
     var textWayValue: String = ""
+    var textMessage: String = ""
+    var delegate: EditDelegate?
+    var isOn: Bool = false
+    var isZoom: Bool = false {
+        // isZoom값이 바뀔때마다 버튼의 타이틀 변경하기
+        didSet {
+            guard btnImgZoom != nil else {
+                return
+            }
+            guard oldValue != isZoom else {
+                return
+            }
+            
+            setBtnImgZoomTitle()
+        }
+    }
 
     @IBOutlet var lblWay: UILabel!
+    @IBOutlet var txMessage: UITextField!
+    @IBOutlet var swlsOn: UISwitch!
+    @IBOutlet var btnImgZoom: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         lblWay.text = textWayValue
+        txMessage.text = textMessage
+        swlsOn.isOn = isOn
+        setBtnImgZoomTitle()
     }
     
-    @IBAction func btnDone(_ sender: UIButton) {
-        _ = navigationController?.popViewController(animated: true)
+    // 수정완료버튼을 눌렀을 때 수신자의 메소드를 호출해 위임업무 수행
+    @IBAction func btnEditDone(_ sender: UIButton) {
+        if delegate != nil {
+            delegate?.didMessageEditDone(self, message: txMessage.text!)
+            delegate?.didImageOnOffDone(self, isOn: isOn)
+            delegate?.didImageZoomDone(self, isZoom: isZoom)
+        }
+        
+        _ = navigationController?.popViewController(animated: true) // 이전 화면으로 돌아가기 위한 함수
+    }
+    
+    @IBAction func swImageOnOff(_ sender: UISwitch) {
+        isOn = sender.isOn
+    }
+    
+    @IBAction func changeImgZoom(_ sender: UIButton) {
+        isZoom = !isZoom
+    }
+    
+    private func setBtnImgZoomTitle() {
+        if isZoom {
+            btnImgZoom.setTitle("확대", for: .normal)
+        } else {
+            btnImgZoom.setTitle("축소", for: .normal)
+        }
     }
     
     /*
